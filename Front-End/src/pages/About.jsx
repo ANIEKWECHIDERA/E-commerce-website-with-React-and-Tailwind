86e004e84d870f5f98a2c7ff79b3ebe36a632aaf
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2024-09-02 13:54:32 +0100

    Working On the About Page

--- Front-End/src/pages/About.jsx
+++ Front-End/src/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-2xl text-centerpt-8 border-t"></div>
+    </div>
+  );
 };
 
 export default About;
