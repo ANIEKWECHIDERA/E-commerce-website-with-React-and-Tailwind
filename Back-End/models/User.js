0d2506708969931533a93eec849aeee48afb9030
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2024-09-03 23:44:19 +0100

    Created User model

---
+++ Back-End/models/User.js
@@ -0,0 +1,51 @@
+"use strict";
+//import requred dependencies
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+//define user schema object
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+//hashing the pass word to ensure security
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User, userSchema");
+
+module.exports = User;
