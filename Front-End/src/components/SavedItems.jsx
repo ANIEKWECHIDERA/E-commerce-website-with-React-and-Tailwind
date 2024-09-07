4c6e5a42c271bd42e529cae28bbbd352591aad66
ANIEKWECHIDERA <c.hideraaniekwederah73@gmail.com>
2024-09-08 00:46:32 +0100

    Fixed bug and added mobile responsiveness

--- Front-End/src/components/SavedItems.jsx
+++ Front-End/src/components/SavedItems.jsx
@@ -8,23 +8,19 @@ const SavedItems = () => {
   const productSlice = products.slice(0, 6);
 
   return (
-    <div className=" shadow-lg pb-5 px-4 flex flex-col items-center w-[90%] m-auto  gap-4 text-gray-800">
+    <div className=" shadow-lg pb-5 sm:px-4 sm:flex sm:flex-col items-center w-[90%] m-auto  gap-4 text-gray-800">
       <div className="items-center  text-left mt-3">
         <p className=" text-3xl">Saved Items</p>
       </div>
       <hr className="w-full my-5" />
-      <div className="grid grid-cols-3 gap-10 p-4 justify-items-center">
+      <div className="w-full sm:grid sm:grid-cols-3 gap-10 p-4 justify-items-center">
         {productSlice.map((item, index) => (
           <div>
             <div
               key={index}
               className="flex flex-col items-center cursor-pointer gap-4 hover:shadow-lg p-4 rounded-sm"
             >
-              <img
-                className="w-40 mb-6"
-                src={item.image[0]}
-                alt="product image"
-              />
+              <img className="w-40 mb-6" src={item.image} alt="product image" />
               <div>
                 <p className="text-sm mb-2">{item.name}</p>
                 <p className="text-gray-600">
